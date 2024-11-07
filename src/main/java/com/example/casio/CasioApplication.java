46f891546968ba5e6f91acb95869b8f6a34a3fd2
kasjke <kasmatuy2k17@gmail.com>
2024-11-07 22:47:25 +0200

    basic

---
+++ src/main/java/com/example/casio/CasioApplication.java
@@ -0,0 +1,13 @@
+package com.example.casio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/casio/CasioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.casio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

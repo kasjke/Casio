9555cd18d3ea89b7b777d6a71271c8102c19a025
Олексій <aleksdenni@gmail.com>
2024-11-13 23:47:45 +0200

    change mapper realization

--- src/main/java/com/example/casio/mapper/ProductMapper.java
+++ src/main/java/com/example/casio/mapper/ProductMapper.java
@@ -3,24 +3,14 @@ package com.example.casio.mapper;
 import com.example.casio.dto.request.ProductRequestDto;
 import com.example.casio.dto.response.ProductResponseDto;
 import com.example.casio.model.Product;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
-@Component
-public class ProductMapper {
-    public Product toModel(ProductRequestDto productRequestDto){
-        return Product.builder()
-                .name(productRequestDto.name())
-                .price(productRequestDto.price())
-                .description(productRequestDto.description())
-                .build();
-    }
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
 
-    public ProductResponseDto toResponseDto(Product product){
-        return ProductResponseDto.builder()
-                .id(product.getId())
-                .name(product.getName())
-                .price(product.getPrice())
-                .description(product.getDescription())
-                .build();
-    }
+    public Product toModel(ProductRequestDto productRequestDto);
+
+    public ProductResponseDto toResponseDto(Product product);
+
 }

5a73064b01057d34e274a2ba98a31b1a8cd95b0a
Олексій <aleksdenni@gmail.com>
2024-11-14 22:38:41 +0200

    add model product

--- src/main/java/com/example/casio/model/Product.java
+++ src/main/java/com/example/casio/model/Product.java
@@ -1,20 +1,27 @@
 package com.example.casio.model;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor(force = true)
 @AllArgsConstructor
+@Table(name = "products")
+@Entity
 public class Product {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
     private Long id;
-    @NotEmpty
+    @Column(name = "name")
     private String name;
-    @NotEmpty
+    @Column(name = "price")
     private Long price;
+    @Column(name = "description")
     private String description;
 
 }

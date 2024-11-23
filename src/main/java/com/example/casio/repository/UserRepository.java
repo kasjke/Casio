9a07b2f6767c6d8fa6149d3aed26c1c5da54fdaf
Олексій <aleksdenni@gmail.com>
2024-11-24 01:08:48 +0200

    add user

--- src/main/java/com/example/casio/repository/UserRepository.java
+++ src/main/java/com/example/casio/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.example.casio.repository;
 
-import com.example.casio.model.Cart;
+import com.example.casio.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartRepository extends JpaRepository<Cart, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
 }
